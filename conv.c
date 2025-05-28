d2eac3bf33d9588881a59db3e4d54537c96887fd
Ferriit <ferriitalt@gmail.com>
2025-05-28 11:10:06 +0200

    Create conv.c

---
+++ conv.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int atoi(const char* str) {
+    return (int)strtol(str, NULL, 10);
+}
+
+char[] itoa(int Num) {
+    char* returnval = malloc(sizeof(char) * 20);
+    snprintf(returnval, 20, "%d", Num);
+    return returnval;
+}
